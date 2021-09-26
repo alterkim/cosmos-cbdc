bc7136c682283261ccce9852c1944db129c32ac0
alterkim <alter@kakao.com>
2021-09-27 02:18:42 +0900

    FEAT: Change UI

--- CBDC_user/src/Pages/AffiliatePage/ACBDCPage.js
+++ CBDC_user/src/Pages/AffiliatePage/ACBDCPage.js
@@ -1,10 +1,12 @@
-import React from "react"
+import React, {useState} from "react"
 import styled from "styled-components"
-import { faChevronLeft, faHome, faBars } from '@fortawesome/free-solid-svg-icons'
+import { faChevronLeft, faHome, faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { history } from '../../_helpers';
 
 const ACBDCPage = ({affiliateInfo}) => {
+    const [modalcommon, setModalcommon] = useState(false)
+
     return (
         <div>
             <Header>
@@ -37,31 +39,37 @@ const ACBDCPage = ({affiliateInfo}) => {
                                 {affiliateInfo.common_cbdc_balance &&affiliateInfo.common_cbdc_balance.toLocaleString()} <span style={{fontSize: '4vw'}}>D-KRW</span>
                             </div>
                         </div>
-                        <div style={{display: 'flex', alignItems: 'center', marginTop: 10}}>
-                            <Button1 
-                                style={{marginLeft: 'auto'}}
-                                onClick={() => history.push('/affiliate/Exchange')}
-                            >교환</Button1>
-                            <Button1 
-                                style={{marginLeft: 5}}
-                                onClick={() => history.push('/affiliate/transfer')}
-                            >
-                                이체
+                        <div style={{display: 'flex', alignItems: 'center', marginTop:10}}>
+                            <Button1
+                                style={{marginLeft:'auto'}}
+                                onClick={() => setModalcommon(true)}
+                            >거래선택
                             </Button1>
-                            <Button1 
-                                style={{
-                                    marginLeft: 5, 
-                                    marginRight: 20
-                                }}
+                            <Button1
+                                style={{marginLeft: 45, marginRight: 27}}
                                 onClick={() => history.push('/affiliate/deal/cbdc')}
-                            >
-                                거래내역
+                            >거래내역
                             </Button1>
                         </div>
                     </CardChild>
                 </CardBody>
             </Body>
-
+            {modalcommon && <Modal>
+                <ModalBackground onClick={() => setModalcommon(false)}></ModalBackground>
+                <ModalContent>
+                    <ModalHeader>
+                        <div style={{marginLeft: 180, fontSize: '4.5vw'}}>거래 선택</div>
+                        <FontAwesomeIcon icon={faTimes} style={{color: "#000", fontSize: '4vw', marginRight:20, cursor: 'pointer'}} onClick={() => setModalcommon(false)}/>
+                    </ModalHeader>
+                    <Mline/>
+                    <MButton onClick={() => history.push('/affiliate/Exchange')}>교환</MButton>
+                    <Mline/>
+                    <MButton onClick={() => history.push('/affiliate/transfer')}>이체</MButton>
+                    <Mline/>
+                    <MButton onClick={() => history.push('/affiliate/cancel')}>결제 취소 승인/거절</MButton>
+                    <Mline/>
+                </ModalContent>
+                </Modal>}
         </div>
     )
 }
@@ -139,22 +147,80 @@ const CardChildName = styled.div`
 `
 const Button1 = styled.button`
     color: #ffffff;
-    height: 4.19vh;
-    border-radius: 2vh;
+    height: 4vh;
+    border-radius: 2.5vh;
     border: none;
     background-color: #00b2a7;
     font-size: 3.6vw;
     outline: none;
-    padding: 0.5vw 1.5vh;
+    padding: 0.5vw 4.0vh;
     cursor : pointer;
 `
 const Button2 = styled.button`
-    color: #adacac;
-    min-height: 4.19vh;
-    border-radius: 2vh;
-    background-color: #ffffff;
-    border: 1px solid #adacac;
-    outline: none;
+    color: #ffffff;
+    height: 4vh;
+    border-radius: 2.5vh;
+    border: none;
+    background-color: #00b2a7;
     font-size: 3.6vw;
-    padding: 0.5vw 1.5vh;
+    outline: none;
+    padding: 0.5vw 2.5vh;
+    cursor : pointer;
+`
+const Modal = styled.div`
+    position: fixed;
+    z-index: 1000;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+`
+const ModalContent = styled.div`
+    top: 70%;
+    height: 80%;
+    width: 95%;
+    border-radius:5px;
+    position: fixed;
+    z-index: 1000;
+    font-size: 13px;
+    background-color: #ffffff;
+    transfrom: translate(-50%,-50%);
+    overflow-y: auto;
+    margin-left: 10px;
+`
+const ModalBackground = styled.div`
+    position: fixed;
+    z-index: 1000;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    background-color: #000000;
+    opacity: 0.4;
+`
+const ModalHeader = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 6.08vh;
+    background-color: #fff;
+    color: #000;
+    font-size: 3.7vw;
+    font-weight: 600;
+    font-weight: 600;
+`
+const Mline = styled.div`
+    height: 1.4px;
+    width: 110%;
+    background-color: #808080;
+`
+const MButton = styled.div`
+    width: 100%;
+    color: #000000;
+    height: 2vh;
+    background-color: #ffffff;
+    font-size: 3.5vw;
+    outline: none;
+    padding: 3.5vw 2.5vh;
+    cursor: pointer;
 `
