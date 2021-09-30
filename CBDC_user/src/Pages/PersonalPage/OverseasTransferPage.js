0335faeb73c13a26c8992b02ee486500953a4e52
alterkim <alter@kakao.com>
2021-09-30 16:16:27 +0900

    FEAT: Add 해외송금 button in main page

---
+++ CBDC_user/src/Pages/PersonalPage/OverseasTransferPage.js
@@ -0,0 +1,46 @@
+import { faChevronLeft, faHome, faBars } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import React from "react"
+import styled from "styled-components"
+import { history } from "../../_helpers"
+
+const OverseasTransferpage = ({userInfo}) => {
+
+    return (
+        <div>
+            <Header>
+                <FontAwesomeIcon
+                    icon={faChevronLeft}
+                    style={{color: "#000", fontSize: '4vw', marginLeft: '5vw', cursor: 'pointer'}}
+                    onClick={() => history.push('/personal')}/>
+                <HeaderText style={{marginLeft: 40}}>해외송금</HeaderText>
+                <div>
+                    <FontAwesomeIcon icon={faHome} style={{color: "#000", fontSize: '4vw', marginRight: 15}}/>
+                    <FontAwesomeIcon icon={faBars} style={{color: "#000", fontSize: '4vw', marginRight: '5vw'}}/>
+                </div>
+            </Header>
+        </div>
+    )
+}
+
+export { OverseasTransferpage}
+
+const Header = styled.div`
+    background-color: #fff;
+    height: 6.76vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    position: relative;
+    font-size: 4vw;
+    font-weight: 600;
+`
+const Body = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+`
+const HeaderText = styled.div`
+    color: #000;
+`
