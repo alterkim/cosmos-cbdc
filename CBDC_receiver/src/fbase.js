747c2a54ae821d6f63c0de646d4a6e0b90b66422
alterkim <alter@kakao.com>
2021-10-25 16:15:58 +0900

    FIX: Temporary firebase connection API

--- CBDC_receiver/src/fbase.js
+++ CBDC_receiver/src/fbase.js
@@ -5,25 +5,20 @@ import "firebase/storage";
 import "firebase/analytics"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCSl6umMzO_KuVpSvltljlSn0RZXF55Uqg",
-    authDomain: "hana-cbdc.firebaseapp.com",
-    projectId: "hana-cbdc",
-    storageBucket: "hana-cbdc.appspot.com",
-    messagingSenderId: "783782723412",
-    appId: "1:783782723412:web:5ef3d75feff4709196d2c1",
-    measurementId: "G-71S4Y5PH75"
-    // apiKey: "AIzaSyDCgtdgWyKEWrjfDzjgnqfrzcgsvUnSxJ0",
-    // authDomain: "hana2-4fe43.firebaseapp.com",
-    // projectId: "hana2-4fe43",
-    // storageBucket: "hana2-4fe43.appspot.com",
-    // messagingSenderId: "481965009550",
-    // appId: "1:481965009550:web:7f7119d6ae91ed08b737df"
-    // apiKey: "AIzaSyDRHM9F5V0uk8xvcyMloyVni0sgFmXvynQ",
-    // authDomain: "hana-fb0c7.firebaseapp.com",
-    // projectId: "hana-fb0c7",
-    // storageBucket: "hana-fb0c7.appspot.com",
-    // messagingSenderId: "327287366295",
-    // appId: "1:327287366295:web:791983ff52731efa17bfd2"
+    // apiKey: "AIzaSyCSl6umMzO_KuVpSvltljlSn0RZXF55Uqg",
+    // authDomain: "hana-cbdc.firebaseapp.com",
+    // projectId: "hana-cbdc",
+    // storageBucket: "hana-cbdc.appspot.com",
+    // messagingSenderId: "783782723412",
+    // appId: "1:783782723412:web:5ef3d75feff4709196d2c1",
+    // measurementId: "G-71S4Y5PH75"
+    apiKey: "AIzaSyAosfEbUKtEIb20RZSJGPvKEmSnSoIWVH4",
+    authDomain: "hana-cbdc2.firebaseapp.com",
+    projectId: "hana-cbdc2",
+    storageBucket: "hana-cbdc2.appspot.com",
+    messagingSenderId: "342269223222",
+    appId: "1:342269223222:web:6c367ce8e24320a2045397",
+    measurementId: "G-6KCXMHTMC3"
   };
 
 
